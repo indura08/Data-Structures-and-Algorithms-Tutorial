b278c2590e8162673977ff7dc89b0b9076063d60
indura08 <induraperera3@gmail.com>
2025-01-13 00:44:07 +0530

    new works with linked lists

--- LinkedListsPractice.java
+++ LinkedListsPractice.java
@@ -20,10 +20,29 @@ public class LinkedListsPractice {
         //clubs.poll();
         //clubs.pop();
 
-        String name = clubs.get(0);
-        System.out.println(name);
+        //String name = clubs.get(0);
+        //System.out.println(name);
 
-        System.out.println(clubs);
+        //System.out.println(clubs);
+
+        //how to access previous and next elements using linkedList class?
+        // we do it using a ListIterator class. how? :- The ListIterator class in Java is a special type of iterator that allows bidirectional traversal of a list (both forward and backward). It’s specifically designed for use with list-based collections like ArrayList and LinkedList.
+
+        ListIterator<String> iterator = clubs.listIterator();
+
+        System.out.println("Traversing forward");
+
+        while(iterator.hasNext()){
+            String nextElement = iterator.next();
+            System.out.println("Next element " + nextElement);
+
+            if(iterator.hasPrevious()){
+                String previousElement = iterator.previous();
+                System.out.println("Previous element " + previousElement);
+            }
+
+            iterator.next();        //mena me scn eke idl poddk issrht blgnna
+        }
 
 
     }
