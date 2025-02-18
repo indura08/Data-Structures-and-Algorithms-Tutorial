4508bc60488201961f6e4546ff8c7c678c871421
indura08 <induraperera3@gmail.com>
2025-02-19 01:34:00 +0530

    quick sort implementation with c#

--- c-sharp-dsa-implementation-codes/Data-Structures-and-Algorithms/Program.cs
+++ c-sharp-dsa-implementation-codes/Data-Structures-and-Algorithms/Program.cs
@@ -7,14 +7,25 @@ namespace Data_Structures_and_Algorithms
     {
         static void Main(string[] args)
         {
-            int[] arrayToBeSorted = { 9, 3, 1, 8, 6, 5, 7, 4, 2 };
+            int[] arrayToBeSorted = {   738, 123, 987, 564, 321, 876, 432, 100, 765, 901,
+                                        654, 345, 789, 234, 567, 890, 456, 678, 210, 890,
+                                        112, 223, 334, 445, 556, 667, 778, 889, 990, 101,
+                                        202, 303, 404, 505, 606, 707, 808, 909, 111, 222,
+                                        333, 444, 555, 666, 777, 888, 999, 131, 242, 353,
+                                        464, 575, 686, 797, 808, 919, 929, 939, 949, 959,
+                                        969, 979, 989, 999, 111, 121, 131, 141, 151, 161,
+                                        171, 181, 191, 201, 211, 221, 231, 241, 251, 261,
+                                        271, 281, 291, 301, 311, 321, 331, 341, 351, 361,
+                                        371, 381, 391, 401, 411, 421, 431, 441, 451, 461 };
 
             //BubbleSort.BubbleSortAlgorithm(arrayToBeSorted);
             //SelectionSort.SelectionSortAlgorithm(arrayToBeSorted);
 
             //InsertionSort.InsertionSortAlgorithm(arrayToBeSorted);
 
-            MergeSort.MergeSortAlgorithm(arrayToBeSorted);
+            //MergeSort.MergeSortAlgorithm(arrayToBeSorted);
+
+            QuickSort.QuickSortAlgorithm(arrayToBeSorted, 0, arrayToBeSorted.Length - 1);
 
             for (int i = 0; i <= arrayToBeSorted.Length - 1; i++)
             {
--- c-sharp-dsa-implementation-codes/Data-Structures-and-Algorithms/obj/Debug/net8.0/Data-Structures-and-Algorithms.AssemblyInfo.cs
+++ c-sharp-dsa-implementation-codes/Data-Structures-and-Algorithms/obj/Debug/net8.0/Data-Structures-and-Algorithms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data-Structures-and-Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a94cf5e37f9882fdd5eafa6ad01a0aefcea174d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce2460a9c6f02aef817d32f0acdf95632356412")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data-Structures-and-Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data-Structures-and-Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
