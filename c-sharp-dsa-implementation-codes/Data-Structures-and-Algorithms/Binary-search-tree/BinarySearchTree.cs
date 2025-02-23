23ccd11eab8d3548d75d742eb614f72c53b0e6a4
indura08 <induraperera3@gmail.com>
2025-02-23 21:49:24 +0530

    adding insert method implementation for binary search tree data structure

---
+++ c-sharp-dsa-implementation-codes/Data-Structures-and-Algorithms/Binary-search-tree/BinarySearchTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_and_Algorithms.Binary_search_tree
+{
+    class BinarySearchTree
+    {
+        BSTNode root;
+
+        public void Insert(BSTNode newNode)
+        {
+            root = InsertHelper(root, newNode);
+        }
+
+        public BSTNode InsertHelper(BSTNode root, BSTNode newNode)
+        {
+            int data = newNode.data;
+
+            if (root == null)
+            {
+                root = newNode;
+                return root;
+            }
+            else if (root.data < newNode.data)
+            {
+                root.right = InsertHelper(root.right, newNode);
+            }
+            else 
+            {
+                root.left = InsertHelper(root.left, newNode);
+            }
+
+            return root;
+        }
+    }
+}
