76101e64babcd151604719b0684b8df0d2dfe0b4
indura08 <induraperera3@gmail.com>
2025-12-28 01:33:37 +0530

    heap sort ts file without code(excution learned)

---
+++ dsa-javascript/heapSort.ts
@@ -0,0 +1,9 @@
+function heapSort(array:number[]):void{
+    //yet to implement
+}
+
+function heapify(array:number[], heapSize:number, rootIndex:number):void{
+    let largest = rootIndex;
+    //yet to implement
+
+}
