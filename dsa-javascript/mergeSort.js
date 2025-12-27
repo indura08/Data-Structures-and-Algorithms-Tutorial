7f1fc12c7d0e574de13149fec3da65ea25517acc
indura08 <induraperera3@gmail.com>
2025-12-27 14:05:32 +0530

    new work related to mereg sort

--- dsa-javascript/mergeSort.js
+++ dsa-javascript/mergeSort.js
@@ -1,27 +1,54 @@
 function mergeSort(array) {
-    var length = array.length - 1;
+    var length = array.length;
     //base case for this 
     if (length <= 1)
         return;
-    var middle = Math.abs(length / 2);
+    var middle = Math.floor(length / 2);
     //adding to the left array
     var leftArray = [];
     var rightArray = [];
+    //mka array.slice() daalath puluwan
     for (var i = 0; i < middle; i++) {
         leftArray[i] = array[i];
     }
-    for (var j = 0; j < length; j++) {
-        rightArray[j + middle] = array[middle + j];
+    //mekath array.slice() daalath puluwan
+    for (var j = 0; j < length - middle; j++) {
+        rightArray[j] = array[middle + j];
     }
-    leftArray.forEach(function (element) {
-        console.log("leftArray elements: ".concat(element));
-    });
-    rightArray.forEach(function (element) {
-        console.log("rightArray elements: ".concat(element));
-    });
-    array.forEach(function (element) {
-        console.log("array elements: ".concat(element));
-    });
+    mergeSort(leftArray);
+    mergeSort(rightArray);
+    merge(leftArray, rightArray, array);
 }
-var array = [78, 3, 12, 4, 8, 90, 56];
+function merge(leftArray, rightArray, array) {
+    var l = 0, r = 0, a = 0;
+    //mereging all the elements from leftarray to main array and right array  main array
+    while (l < leftArray.length && r < rightArray.length) {
+        if (leftArray[l] < rightArray[r]) {
+            array[a] = leftArray[l];
+            l++;
+            a++;
+        }
+        else {
+            array[a] = rightArray[r];
+            a++;
+            r++;
+        }
+    }
+    //now we hvae to merge remaining elements of the two arrays left and right (because in above while loop we compare leftarray and rightarray and after that there can be some remaining elemets inside both arrays)
+    //so this is for left arrays remining elements
+    while (l < leftArray.length) {
+        array[a] = leftArray[l];
+        a++;
+        l++;
+    }
+    while (r < rightArray.length) {
+        array[a] = rightArray[r];
+        a++;
+        r++;
+    }
+}
+var array = [78, 3, 12, 4, 8, 90, 56, 33, 89, 34, 90, 54, 19, 67, 54, 7, 77, 88, 91, 15, 62, 32, 49, 44, 54, 67, 69, 42, 95, 86, 39, 17, 9, 37, 86];
 mergeSort(array);
+array.forEach(function (element) {
+    console.log("The array after sort is : ".concat(element));
+});
--- dsa-javascript/mergeSort.ts
+++ dsa-javascript/mergeSort.ts
@@ -1,9 +1,9 @@
 function mergeSort(array:number[]):void{
-    const length = array.length - 1;
+    const length = array.length;
     //base case for this 
     if(length <= 1) return;
 
-    let middle = Math.abs(length/2);
+    let middle = Math.floor(length/2);
 
     //adding to the left array
     let leftArray:number[] = [];
@@ -15,21 +15,9 @@ function mergeSort(array:number[]):void{
     }
 
     //mekath array.slice() daalath puluwan
-    for(let j = 0; j<length; j++){
-        rightArray[j + middle] = array[middle + j];
+    for(let j = 0; j<length-middle; j++){
+        rightArray[j] = array[middle + j];
     }
-
-    leftArray.forEach((element:number) => {
-        console.log(`leftArray elements: ${element}`)
-    })
-
-    rightArray.forEach((element:number) => {
-        console.log(`rightArray elements: ${element}`)
-    })
-
-    array.forEach((element:number) => {
-        console.log(`array elements: ${element}`)
-    })
 
     mergeSort(leftArray);
     mergeSort(rightArray);
@@ -38,7 +26,40 @@ function mergeSort(array:number[]):void{
 
 function merge(leftArray:number[], rightArray:number[], array:number[]){
     
+    let l = 0, r = 0, a=0
+    //mereging all the elements from leftarray to main array and right array  main array
+    while(l < leftArray.length && r < rightArray.length){
+        if(leftArray[l]! < rightArray[r]!){
+            array[a] = leftArray[l];
+            l++;
+            a++;
+        }
+        else {
+            array[a]! = rightArray[r];
+            a++;
+            r++;
+        }
+    }
+
+    //now we hvae to merge remaining elements of the two arrays left and right (because in above while loop we compare leftarray and rightarray and after that there can be some remaining elemets inside both arrays)
+    //so this is for left arrays remining elements
+    while(l < leftArray.length){
+        array[a] = leftArray[l];
+        a++;
+        l++;
+    }
+
+    while(r < rightArray.length){
+        array[a] = rightArray[r];
+        a++;
+        r++;
+    }
+
 }
 
-let array:number[] = [78,3,12,4,8,90,56];
+let array:number[] = [78,3,12,4,8,90,56,33,89,34,90,54,19,67,54,7,77,88,91,15,62,32,49,44,54,67,69,42,95,86,39,17,9,37,86];
 mergeSort(array);
+
+array.forEach((element:number) => {
+    console.log(`The array after sort is : ${element}`)
+})
